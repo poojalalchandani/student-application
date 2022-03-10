e95ea1bc46e9236b479ff3a7ea56c22feab4d5ec
poojalalchandani <101362983+poojalalchandani@users.noreply.github.com>
2022-03-10 15:23:18 -0500

    Add files via upload

---
+++ dataproggassg2/Login.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dataproggassg2
+{
+    internal class Login
+    {
+        public void login()
+        {
+            Console.WriteLine("Enter your EmailId");
+            string Emailid = Console.ReadLine();
+            Console.WriteLine("Enter your Password");
+            string Password = Console.ReadLine();
+            Console.WriteLine("Login Succesfull");
+
+
+
+
+        }
+    }
+}
